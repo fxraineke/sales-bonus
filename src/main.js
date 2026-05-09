5c77caa542668dadded8fdf75112d2befff5666c
Pavel Borisov <prborisov17@yandex.ru>
2026-05-09 20:41:06 +0800

    fix Проверка входных данных

--- src/main.js
+++ src/main.js
@@ -41,8 +41,8 @@ function calculateBonusByProfit(index, total, seller) {
 function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
     if (!data
-        || (!Array.isArray(data.sellers) && !Array.isArray(data.products) && !Array.isArray(data.purchase_records))
-        || (data.sellers.length === 0 && data.products.length === 0 && data.purchase_records.length === 0)) {
+        || (!Array.isArray(data.sellers) || !Array.isArray(data.products) || !Array.isArray(data.purchase_records))
+        || (data.sellers.length === 0 || data.products.length === 0 || data.purchase_records.length === 0)) {
         throw new Error('Некорректные входные данные');
     }
 
