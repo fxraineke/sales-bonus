e335af6bfd959a3c8447499b734b6bc52b74571a
Pavel Borisov <prborisov17@yandex.ru>
2026-05-09 20:36:39 +0800

    fix Проверка входных данных

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
     if (!data
         || (!Array.isArray(data.sellers) && !Array.isArray(data.products) && !Array.isArray(data.purchase_records))
-        || (data.sellers.length === 0) && data.products.length === 0 && data.purchase_records.length === 0) {
+        || (data.sellers.length === 0 && data.products.length === 0 && data.purchase_records.length === 0)) {
         throw new Error('Некорректные входные данные');
     }
 
